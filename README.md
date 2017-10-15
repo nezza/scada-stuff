ff32d9b77dff8b66e82cbee923940a2f5eb7ebfc
Thomas Roth <code@stacksmashing.net>
2017-10-15 14:20:49 +0200

    Add Moxa firmware notes

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # scada-stuff
+
+## moxa_parse_fw.py
+
+Extracts the firmware images of certain Moxa Mgate and Nport devices. Tested with the Moxa MGate firmwares.
+
+Firmware images compatible with this tool can be found on the Moxa website.
+
+### Usage
+
+```
+moxa_parse_fw.py <firmware_file> <output_directory>
+```
+
+### Output description
+
+The output directory will contain all extracted files in a flat format. The binary firmware itself is saved to `<output_directory>/fw.bin`. It can directly be loaded into Hopper/IDA Pro/Radare2 with the following settings:
+
+```
+Base address: 0x0
+Entry point: 0x0
+File offset: 0x0
+CPU: ARMv6 Little endian
+```
+
+## Moxa NPort W2150 Firmware
+
+The firmware files for the Moxa NPort W2x50 can be loaded directlry into a disassembler with the following settings:
+
+```
+Base address: 0x0
+Entry point: 0x0
+File offset: 0x58
+CPU: ARMv6 Little endian
+```
+
+Note that this only loads the Linux bootlaoder which uncompresses the kernel. The filesystems themselves can be extracted using `binwalk -e`.
+
+Starting with firmware version 2 the firmware is encrypted.
---
+++ moxa_parse_fw.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# All rights reserved
+# Thomas Roth 2017 <code@stacksmashing.net>
+
+import sys
+import struct
+import os
+
+if len(sys.argv) != 3:
+	print "Usage: %s <firmware_image> <output_directory>" % sys.argv[0]
+	sys.exit()
+
+print "Loading firmware: %s" % sys.argv[1]
+
+
+
+
+FILE_TABLE_OFFSET=0x160
+FILE_TABLE_INTERVAL=0x1e0-0x1a0
+
+FILE_INDICATORS = [
+['\x00', '\x05', '"', '1', '\xe1', '\x07', '\x03', '\x07'],
+['\x00', '\x05', '!', '6', '\xe1', '\x07', '\x03', '\x07'],
+['\x00', '\r', '2', '\x1b', '\xe1', '\x07', '\t', '\x01'],
+['\x00', '\r', '4', '\x08', '\xe1', '\x07', '\t', '\x01'],
+]
+
+FILE_METADATA_INDICATOR_SHORT = ['\x00', '\x05']
+FILE_POSITION_OFFSET=0x60
+
+BINARY_FIRMWARE_LOCATION_POINTER = 0x00000054
+
+outdir = sys.argv[2]
+
+
+def readString(myfile):
+    chars = []
+    while True:
+        c = myfile.read(1)
+        if c == chr(0):
+            return "".join(chars)
+        chars.append(c)
+
+class FileInfo:
+	def __init__(self, name, length, position):
+		self.name = name
+		self.length = length
+		self.position = position
+
+	def __unicode__(self):
+		print self.name
+
+	def __str__(self):
+		return self.__unicode__()
+
+files = []
+with open(sys.argv[1], 'rb') as fwf:
+	print "Seek to beginning of file table"
+	fwf.seek(FILE_TABLE_OFFSET, 0)
+
+	while True:
+		pre_string = fwf.tell()	
+		filename = readString(fwf)
+		print "Checking address: 0x%08x" % pre_string
+		
+
+		fwf.seek(pre_string + 0x30, 0)
+
+		d = list(fwf.read(len(FILE_INDICATORS[0])))
+		if not d in FILE_INDICATORS:
+			#if d[:2] != FILE_METADATA_INDICATOR_SHORT:
+			print "\tReached end of file table"
+			break
+
+		print "\tFilename: %s" % filename
+
+		file_length = struct.unpack("<I", fwf.read(4))[0]
+		print "\tFile length: %d" % file_length
+
+		file_position = struct.unpack("<I", fwf.read(4))[0]
+		print "\tFile position: 0x%08x" % (file_position+FILE_POSITION_OFFSET)
+
+		files.append(FileInfo(filename, file_length, file_position+FILE_POSITION_OFFSET))
+
+
+	print "Number of files: %d" % len(files)
+
+	for file in files:
+		print "Writing to: %s" % file.name
+		path = os.path.join(outdir, file.name)
+		path_dir = os.path.dirname(path)
+		try:
+			os.makedirs(path_dir)
+		except OSError:
+			if not os.path.isdir(path_dir):
+				raise
+		with open(os.path.join(outdir, file.name), "w") as outfile:
+			fwf.seek(file.position, 0)
+			data = fwf.read(file.length)
+
+			outfile.write(data)
+
+	OTHER_DATA_START = 0x00028b40
+	# Get position of binary data
+	fwf.seek(BINARY_FIRMWARE_LOCATION_POINTER)
+	binary_firmware_location = struct.unpack("<I", fwf.read(4))[0] + 0x20
+	print "Firmware is at: 0x%08x" % binary_firmware_location
+
+	# header
+	fwf.seek(binary_firmware_location, 0)
+
+	# Binary data
+	fwf.seek(binary_firmware_location, 0)
+	with open(os.path.join(outdir, "fw.bin"), "w") as fwbin_file:
+		fwbin = fwf.read()
+		fwbin_file.write(fwbin)
